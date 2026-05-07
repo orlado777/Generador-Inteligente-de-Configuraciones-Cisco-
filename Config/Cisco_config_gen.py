7983f9248b417edc6a2fd76b45a46e6450c80fdc
Alexis Ponce <alexis.ponce@inacapmail.cl>
2026-05-07 12:14:12 -0400

    Agrega README inicial del proyecto

---
+++ Config/Cisco_config_gen.py
@@ -0,0 +1,15 @@
+from src.prompts import SYSTEM_PROMPT
+
+def generar_configuracion():
+    print("Generando configuracion...")
+
+def main():
+
+    print("=== GENERADOR CISCO IOS ===")
+
+    entrada = input("Describe el escenario: ")
+
+    generar_configuracion()
+
+if __name__ == "__main__":
+    main()
